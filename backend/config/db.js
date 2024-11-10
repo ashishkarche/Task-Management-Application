a070bc51fb081b178bee45d49cad80c83b688375
ashishkarche <ashishkarche9@gmail.com>
2024-11-11 00:06:06 +0530

    push backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,               
   user: process.env.DB_USER,               
   password: process.env.DB_PASSWORD,       
-  database: process.env.DB_DATABASE,            
+  database: process.env.DB_NAME,            
   ssl: {
     rejectUnauthorized: true,               
     ca: fs.readFileSync(path.join(__dirname, 'isrgrootx1.pem')) // Path to the CA certificate
